eb1cdebd6103e22f48422bfbe6c519b1bd4f21cc
Kévin Ghisi <kevinghisi4@gmail.com>
2021-06-24 13:09:16 -0300

    Getting responsive

--- src/components/Landing/LoginModal.jsx
+++ src/components/Landing/LoginModal.jsx
@@ -1,15 +1,18 @@
 import styles from '../../styles/components/Landing/LoginModal.module.css'
 import { useAppContext } from '../../context/state'
+import { useWindowSize } from '../../utils/getWindowDimensions'
 import { useState } from 'react';
 
 export function LoginModal() {
 
+    const [width] = useWindowSize();
+
     const { closeLogin } = useAppContext();
 
-    const [passwordShown, setPasswordShown] = useState(false);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
     const togglePasswordVisiblity = () => {
-        setPasswordShown(passwordShown ? false : true);
+        setIsPasswordVisible(!isPasswordVisible);
     };
 
     return (
@@ -24,18 +27,28 @@ export function LoginModal() {
 
                     <div className={styles.passwordContainer}>
                         <label htmlFor="password">Password:</label>
-                        <input type="password" name="password" className={styles.passwordInput} />
-                        <img src="icons/eye.svg" alt="" />
+                        <input type={isPasswordVisible ? 'text' : 'password'} name="password" className={styles.passwordInput} />
+                        {!isPasswordVisible ?
+                            <img src="icons/eye.svg" alt="" onClick={togglePasswordVisiblity} />
+                            :
+                            <img src="icons/eye_close.svg" alt="" onClick={togglePasswordVisiblity} />
+                        }
+
+
                     </div>
 
                     <button type="submit" className={styles.submitButton}>Login</button>
 
 
                 </form>
+                {width > 900 ?
+                    <button type="button" onClick={closeLogin} className={styles.closeModal}>
+                        <img src="icons/close.svg" alt="Fechar modal" width={14} height={14} />
+                    </button>
+                    :
+                    <img src="icons/close.svg" alt="Fechar modal" onClick={closeLogin} className={styles.closeModal} />
+                }
 
-                <button type="button" onClick={closeLogin} className={styles.closeModal}>
-                    <img src="icons/close.svg" alt="Fechar modal" width={14} height={14} />
-                </button>
             </div>
         </div>
     )
